935cefda2a3946efd3fe29f69fa7b63bf93b6d4a
SOW_ZOM <o26032002z@gmail.com>
2020-10-28 05:01:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLib;
+//using Hangfire.Server;
+
+namespace pr3_1
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            ClassLib.Worker[] x = new ClassLib.Worker[5];
+            Company y = new ClassLib.Company();
+            x = Program.ReadWorkersArray();
+            Program.Menub(x);
+        }
+    }
+}
