76aae189288e736264b943ccf89d17f8e322521f
SOW_ZOM <o26032002z@gmail.com>
2020-10-28 05:04:57 +0200

    commit1

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace pr3_1
         static void Main(string[] args)
         {
             ClassLib.Worker[] x = new ClassLib.Worker[5];
-            Company y = new ClassLib.Company();
+             Company y = new ClassLib.Company();
             x = Program.ReadWorkersArray();
             Program.Menub(x);
         }
